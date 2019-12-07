ebd9f823d6cf8c1567cb1a66e3ef08519484b0c8
naveenvivek <modernnaveen@gmail.com>
2019-12-07 20:53:13 +0530

    Initial Draft

---
+++ src/test/java/React_with_Springboot/React_with_Springboot/ReactWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package React_with_Springboot.React_with_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
